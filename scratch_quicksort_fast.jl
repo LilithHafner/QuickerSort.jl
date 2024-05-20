05a3955b1056b82bf0059977b0a606c686153384
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-05-20 06:53:23 -0500

    make stable (7% regression)

--- scratch_quicksort_fast.jl
+++ scratch_quicksort_fast.jl
@@ -6,13 +6,13 @@ function _copyto!(dst::AbstractVector, src::AbstractVector, lo::Int, hi::Int)
     src === dst || copyto!(dst, lo, src, lo, hi-lo+1)
 end
 
-function smallsort_to!(dst::AbstractVector, src::AbstractVector, lo::Int, hi::Int, o::Ordering)
+function smallsort_to!(dst::AbstractVector, src::AbstractVector, lo::Int, hi::Int, o::Ordering, rev::Bool)
     @inbounds for i = lo:hi
         j = i
         x = src[i]
         while j > lo
             y = dst[j-1]
-            lt(o, x, y) || break
+            (rev ? !lt(o, y, x) : lt(o, x, y)) || break
             dst[j] = y
             j -= 1
         end
@@ -20,7 +20,7 @@ function smallsort_to!(dst::AbstractVector, src::AbstractVector, lo::Int, hi::In
     end
 end
 
-make_scratch2(v::AbstractVector) = (similar(v), Memory{Tuple{Int, Int, typeof(v), typeof(v)}}(undef, Base.top_set_bit(length(v)-1)))
+make_scratch2(v::AbstractVector) = (similar(v), Memory{Tuple{Int, Int, typeof(v), typeof(v), Bool}}(undef, Base.top_set_bit(length(v)-1)))
 function qs3!(v::AbstractVector, (t,stack) = make_scratch2(v), o::Ordering=Forward)
     stack_size = 0
 
@@ -28,6 +28,7 @@ function qs3!(v::AbstractVector, (t,stack) = make_scratch2(v), o::Ordering=Forwa
 
     src = v
     dst = t
+    rev = false
 
     @inbounds if hi - lo > THRESHOLD()
         while true
@@ -37,43 +38,51 @@ function qs3!(v::AbstractVector, (t,stack) = make_scratch2(v), o::Ordering=Forwa
             pivot_index = mod(hash(lo), lo:hi)
 
             pivot = src[pivot_index]
-            src[pivot_index] = src[hi]
 
             large_values = 0
-            for i in lo:hi-1
+
+            for i in lo:pivot_index-1
                 x = src[i]
-                fx = lt(o, pivot, x)
+                fx = rev ? !lt(o, x, pivot) : lt(o, pivot, x)
                 dst[(fx ? hi : i) - large_values] = x
+                large_values += fx
+            end
+            for i in pivot_index+1:hi
+                x = src[i]
+                fx = rev ? lt(o, pivot, x) : !lt(o, x, pivot)
+                dst[(fx ? hi : i-1) - large_values] = x
                 large_values += fx
             end
 
             new_pivot_index = hi-large_values
 
             if new_pivot_index-lo < THRESHOLD() && hi-new_pivot_index < THRESHOLD()
-                smallsort_to!(v, dst, lo, new_pivot_index-1, o)
-                smallsort_to!(v, dst, new_pivot_index+1, hi, o)
+                smallsort_to!(v, dst, lo, new_pivot_index-1, o, rev)
+                smallsort_to!(v, dst, new_pivot_index+1, hi, o, !rev)
                 stack_size == 0 && (v[new_pivot_index] = pivot; break)
-                lo, hi, src, dst = stack[stack_size]
+                lo, hi, src, dst, rev = stack[stack_size]
                 stack_size -= 1
             elseif new_pivot_index-lo < THRESHOLD()
-                smallsort_to!(v, dst, lo, new_pivot_index-1, o)
+                smallsort_to!(v, dst, lo, new_pivot_index-1, o, rev)
                 lo = new_pivot_index+1
+                rev = !rev
             elseif hi-new_pivot_index < THRESHOLD()
-                smallsort_to!(v, dst, new_pivot_index+1, hi, o)
+                smallsort_to!(v, dst, new_pivot_index+1, hi, o, !rev)
                 hi = new_pivot_index-1
             elseif new_pivot_index-lo < hi-new_pivot_index
-                stack[stack_size += 1] = (new_pivot_index+1, hi, src, dst)
+                stack[stack_size += 1] = (new_pivot_index+1, hi, src, dst, !rev)
                 hi = new_pivot_index-1
             else
-                stack[stack_size += 1] = (lo, new_pivot_index-1, src, dst)
+                stack[stack_size += 1] = (lo, new_pivot_index-1, src, dst, rev)
                 lo = new_pivot_index+1
+                rev = !rev
             end
             dst,src = src,dst
 
             v[new_pivot_index] = pivot
         end
     else
-        smallsort_to!(v, v, lo, hi, o)
+        smallsort_to!(v, v, lo, hi, o, false)
     end
 
     v
