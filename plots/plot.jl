9217a19efc1229879d22de1c8c5f6c70734d24cc
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-05-31 19:40:36 -0500

    update fonts

--- plots/plot.jl
+++ plots/plot.jl
@@ -4,7 +4,10 @@ legendify(s::Symbol) = replace(string(s), "_" => " ", "hafner" => "proposed", "l
 
 function plot_runtime_data(;path="runtime_data.csv")
     data = CSV.read(path, DataFrame)
-    p = plot(xaxis=:log, legend=:topleft, ylabel="Runtime (ns per element)", xlabel="Input size (elements)", xticks=10 .^ (1:8))
+    p = plot(xaxis=:log, legend=:topleft, ylabel="Runtime (ns per element)", xlabel="Input size (elements)", xticks=10 .^ (1:8),
+        title="Runtime to sort uniformly random 64-bit integers", titlefont=font("Computer Modern"),
+        legendfont=font(11, "Computer Modern"), guidefont=font(12, "Computer Modern"),
+        xtickfont=font(12, "Computer Modern"), ytickfont=font(12, "Computer Modern"))
     for (colname, color) in zip(propertynames(data)[2:end], [1,2,3,2,4,3,3])
         marker = startswith(string(colname), "simple") ? :square : colname == :optimized_hafner ? :utriangle : :circle
 
@@ -16,7 +19,10 @@ end
 
 function plot_count_data(;path="count_data.csv")
     data = CSV.read(path, DataFrame)
-    p = plot(xaxis=:log, legend=:topleft, ylabel="Average comparrisons per element", xlabel="Input size (elements)", xticks=10 .^ (1:8))
+    p = plot(xaxis=:log, legend=:topleft, ylabel="Average comparrisons per element", xlabel="Input size (elements)", xticks=10 .^ (1:8),
+        title="Average comparrisons to sort random input", titlefont=font("Computer Modern"),
+        legendfont=font(11, "Computer Modern"), guidefont=font(12, "Computer Modern"),
+        xtickfont=font(12, "Computer Modern"), ytickfont=font(12, "Computer Modern"))
     for (colname, color) in zip(propertynames(data)[2:end], [1,2,3,2,4,3,3,:black])
         marker = startswith(string(colname), "simple") ? :square : colname == :optimized_hafner ? :utriangle : colname == :theoretical_minimum ? :none : :circle
 
