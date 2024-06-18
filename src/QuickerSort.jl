5fd3b864430084a4edbdaf141263542949bfc161
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-06-18 07:35:34 -0500

    Convert from SVG to PDF before latex

--- src/QuickerSort.jl
+++ src/QuickerSort.jl
@@ -6,16 +6,25 @@ include("simple_lomuto.jl")
 include("simple_hafner.jl")
 include("optimized_hafner.jl")
 
-include("../plots/data.jl")
-include("../plots/plot.jl")
+include(joinpath(dirname(@__DIR__), "plots", "data.jl"))
+include(joinpath(dirname(@__DIR__), "plots", "plot.jl"))
 
 function reproduce_figures()
-    @time save_runtime_data()
-    plot_runtime_data()
-    savefig("runtime.svg")
-    @time save_comparrison_data()
-    plot_comparrison_data()
-    savefig("comparrisons.svg")
+    cd(joinpath(dirname(@__DIR__), "paper")) do
+        # @time save_runtime_data()
+        plot_runtime_data()
+        savefig("runtime.svg")
+        # @time save_comparrison_data()
+        plot_comparrison_data()
+        savefig("comparrisons.svg")
+        try
+            run(`rsvg-convert -f pdf -o runtime.pdf runtime.svg`)
+            run(`rsvg-convert -f pdf -o comparrisons.pdf comparrisons.svg`)
+        catch
+            @warn "Failed to convert SVGs to PDFs"
+        end
+        nothing
+    end
 end
 
 end
